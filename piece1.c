f7bbda0c709fdc31c818d9fd733d1de42a69e5c6
mourot_l <leomourot@gmail.com>
2012-10-22 13:59:39 +0200

    Edit Piece v2

--- piece1.c
+++ piece1.c
@@ -3,24 +3,24 @@
 
 void	piece1_rot1(t_env* env)
 {
-	env->piece.x = env->pieces.piece0.x2 - 1;
-	env->piece.x1 = env->pieces.piece0.x2;
-	env->piece.x3 = env->pieces.piece0.x2;
-	env->piece.y = env->pieces.piece0.y2;
-	env->piece.y1 = env->pieces.piece0.y2 + 1;
-	env->piece.y3 = env->pieces.piece0.y2 - 1;
+	env->piece.x = env->piece.x2 - 1;
+	env->piece.x1 = env->piece.x2;
+	env->piece.x3 = env->piece.x2;
+	env->piece.y = env->piece.y2;
+	env->piece.y1 = env->piece.y2 + 1;
+	env->piece.y3 = env->piece.y2 - 1;
 	env->piece.rot = 1;
 }
 
 
 void	piece1_rot2(t_env* env)
 {
-	env->piece.x = env->pieces.piece0.x2;
-	env->piece.x1 = env->pieces.piece0.x2 + 1;
-	env->piece.x3 = env->pieces.piece0.x2 - 1;
-	env->piece.y = env->pieces.piece0.y2 - 1;
-	env->piece.y1 = env->pieces.piece0.y2;
-	env->piece.y3 = env->pieces.piece0.y2;
+	env->piece.x = env->piece.x2;
+	env->piece.x1 = env->piece.x2 + 1;
+	env->piece.x3 = env->piece.x2 - 1;
+	env->piece.y = env->piece.y2 - 1;
+	env->piece.y1 = env->piece.y2;
+	env->piece.y3 = env->piece.y2;
 	env->pieces.piece1.rot = 2;
 }
 
--- piece4.c
+++ piece4.c
@@ -2,22 +2,22 @@
 
 void	piece4_rot1(t_env* env)
 {
-	env->piece.x = env->pieces.piece5.x2;
-	env->piece.x1 = env->pieces.piece5.x2 + 1;
-	env->piece.x3 = env->pieces.piece5.x2 + 1;
-	env->piece.y = env->pieces.piece5.y2 + 1;
-	env->piece.y1 = env->pieces.piece5.y2;
-	env->piece.y3 = env->pieces.piece5.y2 - 1;
+	env->piece.x = env->piece.x2;
+	env->piece.x1 = env->piece.x2 + 1;
+	env->piece.x3 = env->piece.x2 + 1;
+	env->piece.y = env->piece.y2 + 1;
+	env->piece.y1 = env->piece.y2;
+	env->piece.y3 = env->piece.y2 - 1;
 	env->piece.rot = 1;
 }
 
 void	piece4_rot0(t_env* env)
 {
-	env->piece.x = env->pieces.piece5.x2 - 1;
-	env->piece.x1 = env->pieces.piece5.x2;
-	env->piece.x3 = env->pieces.piece5.x2 + 1;
-	env->piece.y = env->pieces.piece5.y2;
-	env->piece.y1 = env->pieces.piece5.y2 - 1;
-	env->piece.y3 = env->pieces.piece5.y2 - 1;
+	env->piece.x = env->piece.x2 - 1;
+	env->piece.x1 = env->piece.x2;
+	env->piece.x3 = env->piece.x2 + 1;
+	env->piece.y = env->piece.y2;
+	env->piece.y1 = env->piece.y2 - 1;
+	env->piece.y3 = env->piece.y2 - 1;
 	env->piece.rot = 0;
 }
