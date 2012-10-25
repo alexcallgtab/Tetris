6b8d94a1387f72bf8211747e51ddc739bb11888e
mourot_l <leomourot@gmail.com>
2012-10-25 10:59:11 +0200

    Changement skin piece

--- piece.c
+++ piece.c
@@ -5,13 +5,13 @@
 void	show_piece(t_env* env)
 {
 	tputs(tgoto(env->cm, env->piece.x, env->piece.y), 1, id_put);
-	id_print_str("\033[01;46m#");
+	id_print_str("\033[01;46m ");
 	tputs(tgoto(env->cm, env->piece.x1, env->piece.y1), 1, id_put);
-	id_print_str("\033[01;46m#");
+	id_print_str("\033[01;46m ");
 	tputs(tgoto(env->cm, env->piece.x2, env->piece.y2), 1, id_put);
-	id_print_str("\033[01;46m#");
+	id_print_str("\033[01;46m ");
 	tputs(tgoto(env->cm, env->piece.x3, env->piece.y3), 1, id_put);
-	id_print_str("\033[01;46m#");
+	id_print_str("\033[01;46m ");
 }
 
 void	erase_screen_piece(t_env* env)
